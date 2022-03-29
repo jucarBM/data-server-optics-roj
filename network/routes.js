fc1e2267143cc8f8f3414d50b0a326b43bb9b6f5
Juan Carlos Barbaran <jucar251096@gmail.com>
2022-03-29 16:57:09 -0500

    separate 3 entities: experiment, instrument, report

--- network/routes.js
+++ network/routes.js
@@ -1,9 +1,11 @@
 const report = require("../components/report/network");
 const experiment = require("../components/experiment/network");
+const instrument = require("../components/instrument/network");
 
 const routes = function (server) {
   server.use("/report", report);
   server.use("/experiment", experiment);
+  server.use("/instrument", instrument);
 };
 
 module.exports = routes;
